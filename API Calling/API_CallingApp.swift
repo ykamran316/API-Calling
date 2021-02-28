69822ff0a4e058f28c185d6438c7a9be5fe0544f
Yawar Kamran <yawarkamran316@gmail.com>
2021-02-28 11:53:34 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Student on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
